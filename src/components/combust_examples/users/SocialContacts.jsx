ff9cdc11269088f84fbb594e1f7b21ad8526a1d4
Joe <mrjoeroddy@gmail.com>
2017-12-12 16:19:35 -0500

    only show social dropdown when >1 social module installed

--- src/components/combust_examples/users/SocialContacts.jsx
+++ src/components/combust_examples/users/SocialContacts.jsx
@@ -20,31 +20,36 @@ export default class SocialContacts extends Component {
       return <span />;
     }
 
+    const socialModes = Object.keys(componentsByMode);
+    const isDropdownShown = socialModes && socialModes.length > 1;
+
     return (
       <div className="SocialContacts uk-position-right">
         <div className="uk-inline">
           <h4>
             {this.state.mode}
-            <span uk-icon="icon: triangle-down"> </span>
+            {isDropdownShown && <span uk-icon="icon: triangle-down"> </span>}
           </h4>
-          <div uk-dropdown="pos: bottom-justify">
-            <ul className="uk-nav uk-dropdown-nav">
-              {Object.keys(componentsByMode).map((mode, i) => {
-                return mode !== this.state.mode ? (
-                  <li key={i}>
-                    <a
-                      onClick={e => {
-                        this.setMode(mode);
-                      }}
-                      href="#"
-                    >
-                      {mode}
-                    </a>
-                  </li>
-                ) : null;
-              })}
-            </ul>
-          </div>
+          {isDropdownShown && (
+            <div uk-dropdown="pos: bottom-justify">
+              <ul className="uk-nav uk-dropdown-nav">
+                {socialModes.map((mode, i) => {
+                  return mode !== this.state.mode ? (
+                    <li key={i}>
+                      <a
+                        onClick={e => {
+                          this.setMode(mode);
+                        }}
+                        href="#"
+                      >
+                        {mode}
+                      </a>
+                    </li>
+                  ) : null;
+                })}
+              </ul>
+            </div>
+          )}
         </div>
         {componentsByMode[this.state.mode]}
       </div>
