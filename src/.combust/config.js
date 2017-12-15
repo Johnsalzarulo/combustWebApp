a98b4cebaa0e435d656c444629cfbdeb162ae159
Joe <mrjoeroddy@gmail.com>
2017-12-15 17:00:06 -0500

    remove defualt firebase config

--- src/.combust/config.js
+++ src/.combust/config.js
@@ -1,9 +1,2 @@
 //Don't accidentally rename this! ;D
-export const firebaseConfig = {
-  apiKey: "AIzaSyBmFff_SfNSBvDplVVXKLlgxRpqRh4mjjw",
-  authDomain: "firestation-e149d.firebaseapp.com",
-  databaseURL: "https://firestation-e149d.firebaseio.com",
-  projectId: "firestation-e149d",
-  storageBucket: "firestation-e149d.appspot.com",
-  messagingSenderId: "951945535511"
-};
+export const firebaseConfig = null;
