ee6c8a2bc48ae05616d59f67236fe79a6ca84a61
Joe <mrjoeroddy@gmail.com>
2017-12-15 16:56:10 -0500

    welcome: table of available firebase apps

--- src/components/combust_examples/Welcome.jsx
+++ src/components/combust_examples/Welcome.jsx
@@ -7,15 +7,22 @@ import usersStore from "../../stores/UsersStore";
 import { stores } from "../../.combust/init";
 import SocialContacts from "./users/SocialContacts";
 import Profile from "./users/Profile";
+const availApps = require("../../.combust/availApps.json");
 
 @observer
 export default class Welcome extends Component {
-  state = {};
+  state = {
+    modalText: null
+  };
 
   componentDidMount() {
     welcomeStore.isFirebaseConfigured();
     welcomeStore.isEmailAuthEnabled();
   }
+
+  setModalText = modalText => {
+    this.setState({ modalText });
+  };
 
   render() {
     let {
@@ -42,20 +49,73 @@ export default class Welcome extends Component {
             completed={firebaseConfigured}
             title="Configure Firebase"
           >
-            You may do this in the terminal with: <code>combust configure</code>
-            <hr className="uk-divider-small" />
-            Or apply the firebase config manually.{" "}
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://console.firebase.google.com/u/0/project/_/overview"
-            >
-              Select your project and click "Add firebase to your webapp".
-            </a>{" "}
-            <br />
-            Apply the JSON object to <code>firebaseConfig</code> in{" "}
-            <code>src/.combust/config.js</code>
+            {availApps && availApps.length > 0 ? (
+              <div>
+                <div>Select a Firebase project to link to this app</div>
+                <table className="uk-table uk-table-small uk-table-striped uk-table-hover">
+                  <caption>Your current apps</caption>
+                  <thead>
+                    <tr>
+                      <th>Project Name</th>
+                      <th>Project ID</th>
+                      <th>Permissions</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {availApps.map(app => {
+                      return (
+                        <tr
+                          key={app.id}
+                          uk-toggle="target: #my-id"
+                          onClick={e => {
+                            this.setModalText(
+                              <span>
+                                Execute from your terminal: <br />
+                                <br />
+                                <code>combust configure {app.id}</code>
+                              </span>
+                            );
+                          }}
+                        >
+                          <td>{app.name}</td>
+                          <td>{app.id}</td>
+                          <td>{app.role}</td>
+                        </tr>
+                      );
+                    })}
+                  </tbody>
+                </table>
+                <hr className="uk-divider-small" />
+                Or:{" "}
+                <a
+                  href="https://console.firebase.google.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Create a new Firebase project
+                </a>
+              </div>
+            ) : (
+              <div>
+                {" "}
+                You may do this in the terminal with:{" "}
+                <code>combust configure</code>
+                <hr className="uk-divider-small" />
+                Or apply the firebase config manually.{" "}
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://console.firebase.google.com/u/0/project/_/overview"
+                >
+                  Select your project and click "Add firebase to your webapp".
+                </a>{" "}
+                <br />
+                Apply the JSON object to <code>firebaseConfig</code> in{" "}
+                <code>src/.combust/config.js</code>
+              </div>
+            )}
           </RenderDropdown>
+
           {firebaseConfigured && (
             <RenderDropdown
               completed={emailAuthEnabled}
@@ -88,7 +148,7 @@ export default class Welcome extends Component {
                       }
                     }}
                   >
-                    Create your account
+                    Create your first user account
                   </Link>
                 </ToDoItem>
                 <ToDoItem completed={adminConfigured}>
@@ -127,6 +187,19 @@ export default class Welcome extends Component {
             )}
         </ul>
         <SocialContacts />
+        <div id="my-id" uk-modal="true">
+          <div className="uk-modal-dialog uk-modal-body">
+            <h2 className="uk-modal-title" />
+            <button
+              class="uk-modal-close-default"
+              type="button"
+              uk-close="true"
+              title="Close"
+              uk-tooltip="true"
+            />{" "}
+            {this.state.modalText}
+          </div>
+        </div>
       </div>
     );
   }
