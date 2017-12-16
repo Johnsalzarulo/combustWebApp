2e7b2950a8aeda3ed563b88fbf5590896b8ee0a5
Joe <mrjoeroddy@gmail.com>
2017-12-15 21:20:58 -0500

    whoops..

--- src/.combust/config.js
+++ src/.combust/config.js
@@ -1,8 +1,1 @@
-export const firebaseConfig = {
-  "apiKey": "AIzaSyBmFff_SfNSBvDplVVXKLlgxRpqRh4mjjw",
-  "databaseURL": "https://firestation-e149d.firebaseio.com",
-  "storageBucket": "firestation-e149d.appspot.com",
-  "authDomain": "firestation-e149d.firebaseapp.com",
-  "messagingSenderId": "951945535511",
-  "projectId": "firestation-e149d"
-}
+export const firebaseConfig = null;
