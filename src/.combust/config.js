1b04ea92a1864a61535884dc3ef9f9e72c5e2804
Joe <mrjoeroddy@gmail.com>
2018-01-03 16:21:40 -0500

    REVERT IF PUSHED

--- src/.combust/config.js
+++ src/.combust/config.js
@@ -1,1 +1,8 @@
-export const firebaseConfig = null;
+export const firebaseConfig = {
+  "apiKey": "AIzaSyBmFff_SfNSBvDplVVXKLlgxRpqRh4mjjw",
+  "databaseURL": "https://firestation-e149d.firebaseio.com",
+  "storageBucket": "firestation-e149d.appspot.com",
+  "authDomain": "firestation-e149d.firebaseapp.com",
+  "messagingSenderId": "951945535511",
+  "projectId": "firestation-e149d"
+}
